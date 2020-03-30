a92de3b295d1a3ee6691366bc91e059f4a9f25af
lenisaputri <lenisaputri98@gmail.com>
2020-03-30 19:34:20 +0700

    maneh

---
+++ Assets/Script/Kode/QuitButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    private Button quitButton;
+
+    void Start()
+    {
+        quitButton = GetComponent<Button>();
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
