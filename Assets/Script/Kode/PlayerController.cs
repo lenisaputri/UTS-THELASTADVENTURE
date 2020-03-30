bbcd5280344cf604a9502d757fd31c4d5c117869
lenisaputri <lenisaputri98@gmail.com>
2020-03-31 02:33:47 +0700

    eror

--- Assets/Script/Kode/PlayerController.cs
+++ Assets/Script/Kode/PlayerController.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private bool facingRight = true;
     public Rigidbody2D rb;
 
+    // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && facingRight)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
-        }
+            Flip();
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !facingRight)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
+            Flip();
         }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
         }
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+        //Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
