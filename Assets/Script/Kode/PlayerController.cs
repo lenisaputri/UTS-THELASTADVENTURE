997fcb788e790d8b3b2b1667c5fc28fe0b4e9303
lenisaputri <lenisaputri98@gmail.com>
2020-03-31 20:45:24 +0700

    bismillah

--- Assets/Script/Kode/PlayerController.cs
+++ Assets/Script/Kode/PlayerController.cs
@@ -4,36 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool facingRight = true;
     public Rigidbody2D rb;
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && facingRight)
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
-            Flip();
-
+            transform.localScale = new Vector2(-1, 1);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && !facingRight)
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
-            Flip();
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
         }
-    }
-
-    void Flip()
-    {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
-        //Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
     }
 }
